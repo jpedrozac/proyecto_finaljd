9ba570eb3565657b38a579b2ccbd473b3d161327
jpedrozac <110734549+jpedrozac@users.noreply.github.com>
2024-02-22 15:22:44 -0500

    Cambios main

--- main.tf
+++ main.tf
@@ -1,27 +1,122 @@
-# Creación del Load Balancer 
-module "load_balancer" {
-  source = "./modules/load_balancer"
+resource "aws_ecs_cluster" "cluster1" {
+  name = "white-hart"
 
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+resource "aws_ecs_task_definition" "service" {
+  family = "service"
+  container_definitions = jsonencode([
+    {
+      name      = "first"
+      image     = "service-first"
+      cpu       = 1
+      memory    = 3
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    },
+    {
+      name      = "second"
+      image     = "service-second"
+      cpu       = 1
+      memory    = 3
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+
+  volume {
+    name      = "service-storage"
+    host_path = "/ecs/service-storage"
+  }
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [us-east-1a, us-east-1b]"
+  }
+}
+resource "aws_ecs_service" "servicio1" {
+  name            = "servicio1db"
+  cluster         = aws_ecs_cluster.cluster1.id
+  task_definition = aws_ecs_task_definition.servicio1.arn
+  desired_count   = 3
+  iam_role        = aws_iam_role.cluster1.arn
+  depends_on      = [aws_iam_role_policy.cluster1]
+
+  ordered_placement_strategy {
+    type  = "binpack"
+    field = "cpu"
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.cluster1.arn
+    container_name   = "servicio1"
+    container_port   = 80
+  }
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [us-east-1a, us-east-1b]"
+  }
+}
+resource "aws_lb" "test" {
+  name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [for subnet in aws_subnet.public : subnet.id]
+
+  enable_deletion_protection = true
+
+  access_logs {
+    bucket  = aws_s3_bucket.lb_logs.id
+    prefix  = "test-lb"
+    enabled = true
+  }
+
+  tags = {
+    Environment = "production"
+  }
+}
+resource "aws_lb_target_group" "test" {
+  name     = "tf-example-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
 }
 
-# Creación de ECS 
-module "ecs" {
-  source = "./modules/ecs"
-
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+resource "aws_lb" "front_end" {
+  # ...
 }
 
-# Configuración de S3 y CloudFront
-module "s3_cloudfront" {
-  source = "./modules/s3_cloudfront"
-
+resource "aws_lb_target_group" "front_end" {
+  # ...
 }
 
-resource "aws_s3_bucket_object" "index_html" {
-  bucket = aws_s3_bucket.main.id
-  key    = "index.html"
-  source = "index.html"
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.front_end.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.front_end.arn
+  }
 }
 
-resource "aws_s3_bucket" "main" {
-  # Configuración del bucket S3
-}
+
