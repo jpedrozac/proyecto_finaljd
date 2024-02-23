c5fd3b9791f8e8f31a34d669eff993707057a50f
DiegoAle0297 <159805801+DiegoAle0297@users.noreply.github.com>
2024-02-23 15:08:51 -0500

    cambio-target

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ resource "aws_lb_target_group" "proyectolbtg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.finaltf.id
+  target_type = "ip"
 }
 
 resource "aws_lb_listener" "proyectolistener" {
