3aeff1d3f1404ecb4f34b5001ea919a0d902ac3e
DiegoAle0297 <159805801+DiegoAle0297@users.noreply.github.com>
2024-02-26 17:15:00 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -70,3 +70,16 @@ data "aws_iam_policy_document" "coe_s3_web_component_virginia" {
   #restrict_public_buckets = true
 
 #}
+#S3 ACL
+  resource "aws_s3_bucket_acl" "exampleacl" {
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
+}
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
