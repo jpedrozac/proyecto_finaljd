233a734b08e99b25ecc744039f98749081484395
DiegoAle0297 <159805801+DiegoAle0297@users.noreply.github.com>
2024-02-28 20:31:16 -0500

    cambio

--- s3.tf
+++ s3.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "example" {
-  bucket = "mybucketjd20240223"
+  bucket = "example"
 
   tags = {
-    Name        = "My bucket"
+    Name        = "example"
     Environment = "Dev"
   }
 }
